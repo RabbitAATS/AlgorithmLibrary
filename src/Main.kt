93259ac4c2a27a77833990483edb0fcce0f35b00
RabbitAATS <rabbit.aats@gmail.com>
2021-12-04 12:40:43 +0900

    軽微な修正．

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,2 @@
 fun main() {
-
-    //val Q = readLine()!!.split(" ").map{it.toLong()}
-    //val a = Q[0]
-    //val b = Q[1]
-    //val mod = Q[2]
-    //println(repeatedSquare(a, b, mod))
-
-    //val k = readLine()!!.toInt()
-    //val list = readLine()!!.split(" ")
-    println(permutations(3))
-    println(permutations(3, 2))
 }
