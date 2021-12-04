b05eebb1ca624c3bc46ef6e6bde25f0fd2b3e83a
RabbitAATS <rabbit.aats@gmail.com>
2021-12-04 17:04:49 +0900

    Make `Util.kt` class.

--- src/IOUtils.kt
+++
@@ -1,8 +0,0 @@
-fun <T> List<T>.print() =
-    println(joinToString(" "))
-
-fun readIntList(): List<Int> =
-    readLine()?.let{ line -> line.split(" ").map{it.toInt()}} ?: emptyList()
-
-fun readList(): List<String> =
-    readLine()?.let{ line -> line.split(" ")} ?: emptyList()
